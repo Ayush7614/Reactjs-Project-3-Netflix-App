cf57474bfac32a5f83d8789e2108184c97e4d24b
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2020-10-06 00:22:12 +0530

    Create Sdata.jsx

---
+++ src/Sdata.jsx
@@ -0,0 +1,37 @@
+const Sdata = [
+    {
+        sanme: "DARK",
+        imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80100172",
+    },
+    {
+        sanme: "extra curricular",
+        imgscr: "https://asianwiki.com/images/4/47/Human_Class-KDH.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80990668",
+        
+    },
+    {
+        sanme: "Stranger Things",
+        imgscr: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80057281",
+
+    },
+    {
+        sanme: "Mirzapur",
+        imgscr: "https://www.talenthouse.com/item/2090833/09e41ec0",
+        title: "A Amazon Original Series",
+        link: "https://www.primevideo.com/detail/The-Vampire-Diaries/0HG2QOPONEONZPP2ZXNMA6M8Q7",
+    },
+    {
+        sanme: "My First-2 love",
+        imgscr: "https://asianwiki.com/File:Because_It%27s_My_First_Love-TP.jpg",
+        title: "A Netflix Original Series",
+        link: "https://www.netflix.com/in/title/81026700",
+    },
+
+];
+
+export default Sdata;
